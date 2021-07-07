6ca00d09a6ad7762a707489b870b09316d9dc067
Meera Sundar <mirage.sundial@gmail.com>
2021-07-07 15:05:50 +0530

    add pad handler

--- basic-tut-3.py
+++ basic-tut-3.py
@@ -1,12 +1,12 @@
-#!/uusr/bin/env python3
+#!/usr/bin/env python3
 import sys
 import gi
-import logger
+import logging
 
 gi.require_version("GLib", "2.0")
 gi.require_version("GObject", "2.0")
 gi.require_version("Gst", "1.0")
-from gi.repository import GLib, GObject, GSt
+from gi.repository import GLib, GObject, Gst
 
 logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)8s] - %(message)s")
 logger = logging.getLogger(__name__)
@@ -53,17 +53,54 @@ class CustomData:
             sys.exit(1)
 
         #Listening for messages:
-        bus = self.pipeline.get_bus()
-        msg = self.bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.STATE_CHANGED | Gst.MessageType.EOS)
+        self.bus = self.pipeline.get_bus()
+        while True:
+            msg = self.bus.timed_pop_filtered(Gst.CLOCK_TIME_NONE, Gst.MessageType.ERROR | Gst.MessageType.STATE_CHANGED | Gst.MessageType.EOS)
 
-        #Parse message
-        if msg:
-            if msg.type == Gst.MessageType.ERROR:
-                err, debug_info = msg.parse_error()
-                logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
-                logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
-            elif msg.type == Gst.MessageType.EOS:
-                logger.info("End-Of-Stream reached.")
-            else:
-                # This should ideally not be reached
-                logger.error("Unexpected message received.")
+            #Parse message
+            if msg:
+                if msg.type == Gst.MessageType.ERROR:
+                    err, debug_info = msg.parse_error()
+                    logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
+                    logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
+                    break
+                elif msg.type == Gst.MessageType.EOS:
+                    logger.info("End-Of-Stream reached.")
+                    break
+
+                elif msg.type == Gst.MessageType.STATE_CHANGED:
+                    if msg.src == self.pipeline:
+                        old_state, new_state, pending_state = msg.parse_state_changed()
+                        print(f"Pipeline state changed from {Gst.Element.state_get_name(old_state)} to {Gst.Element.state_get_name(new_state)}")
+                else:
+                    # This should ideally not be reached
+                    logger.error("Unexpected message received.")
+                    break
+
+    def pad_added_handler(self, src, new_pad):
+        sink_pad = self.convert.get_static_pad("sink")
+        print(f"Received new pad {new_pad.get_name()} from {src.get_name()}")
+
+        if sink_pad.is_linked():
+            print("Already linked")
+            return
+
+        new_pad_caps = new_pad.get_current_caps()
+        new_pad_struct = new_pad_caps.get_structure(0)
+        new_pad_type = new_pad_struct.get_name()
+
+        if not new_pad_type.startswith("audio/x-raw"):
+            print("Type not audio.")
+            return
+
+        #Linking
+        ret = new_pad.link(sink_pad)
+        if ret == Gst.PadLinkReturn.OK:
+            print("Link success")
+        else:
+            print("Link failure")
+
+        return
+
+if __name__ == '__main__':
+    c = CustomData()        
