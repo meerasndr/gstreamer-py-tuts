ec20777c96ef139d166aae9bd8d920d44b6665c1
Meera Sundar <mirage.sundial@gmail.com>
2021-07-23 15:05:33 +0530

    refactor tut 4

--- basic-tut-4.py
+++ basic-tut-4.py
@@ -11,32 +11,47 @@ from gi.repository import GLib, GObject, Gst
 logging.basicConfig(level=logging.DEBUG, format="[%(name)s] [%(levelname)8s] - %(message)s")
 logger = logging.getLogger(__name__)
 
+class CustomData:
+    def __init__(self):
+        # Creat the single element playbin
+        self.playbin = Gst.ElementFactory.make("playbin", "playbin")
+        # Are we in playing state?
+        self.playing = False
+        # Should we terminate execution?
+        self.terminate = False
+        # Is seeking enabled for this media?
+        self.seek_enabled = False
+        # Have we performed the seek already?
+        self.seek_done = False
+        # How long does this media last, in nanoseconds?
+        self.duration = Gst.CLOCK_TIME_NONE
+
 def handle_message(msg):
     if msg.type == Gst.MessageType.ERROR:
         err, debug_info = msg.parse_error()
         logger.error(f"Error received from element {msg.src.get_name()}: {err.message}")
         logger.error(f"Debugging information: {debug_info if debug_info else 'none'}")
-        d.terminate = True
+        data.terminate = True
     elif msg.type == Gst.MessageType.EOS:
         logger.info("End-Of-Stream reached.")
-        d.terminate = True
+        data.terminate = True
     elif msg.type == Gst.MessageType.DURATION_CHANGED:
         # The duration has changed, mark the current one as invalid
-        d.duration = Gst.CLOCK_TIME_NONE
+        data.duration = Gst.CLOCK_TIME_NONE
 
     elif msg.type == Gst.MessageType.STATE_CHANGED:
-            if msg.src == d.playbin:
+            if msg.src == data.playbin:
                 old_state, new_state, pending_state = msg.parse_state_changed()
                 logger.info(f"Pipeline state changed from {Gst.Element.state_get_name(old_state)} to {Gst.Element.state_get_name(new_state)}")
                 # Remember whether we are in the PLAYING state or not
-                d.playing = (new_state == Gst.State.PLAYING)
-                if d.playing: # We just moved to PLAYING. Check if seeking is possible
+                data.playing = (new_state == Gst.State.PLAYING)
+                if data.playing: # We just moved to PLAYING. Check if seeking is possible
                     #create new query object
                     seekquery = Gst.Query.new_seeking(Gst.Format.TIME)
-                    if d.playbin.query(seekquery):
-                        format, d.seek_enabled, start, end = seekquery.parse_seeking()
-                        if d.seek_enabled:
-                            logger.info(f"Seeking is ENABLED {d.seek_enabled} from {start} to {end}")
+                    if data.playbin.query(seekquery):
+                        format, data.seek_enabled, start, end = seekquery.parse_seeking()
+                        if data.seek_enabled:
+                            logger.info(f"Seeking is ENABLED {data.seek_enabled} from {start} to {end}")
                         else:
                             logger.info("Seeking is disabled for this stream\n") # Live video stream?
                     else:
@@ -47,47 +62,13 @@ def handle_message(msg):
 
     return
 
-
-class CustomData:
-    def __init__(data):
-        # Initialize GStreamer
-        # Setup internal path lists, plugins, GstRegistry
-        Gst.init(None)
-
-        # Creat the single element playbin
-        data.playbin = Gst.ElementFactory.make("playbin", "playbin")
-
-        # Are we in playing state?
-        data.playing = False
-        # Should we terminate execution?
-        data.terminate = False
-        # Is seeking enabled for this media?
-        data.seek_enabled = False
-        # Have we performed the seek already?
-        data.seek_done = False
-        # How long does this media last, in nanoseconds?
-        data.duration = Gst.CLOCK_TIME_NONE
-        #message handler method
-        data.handle_message = handle_message
-
-
-if __name__ == '__main__':
-    d = CustomData()
-    if not d.playbin:
+def main():
+    if not data.playbin:
         logger.error("Not all elements could be created")
         sys.exit(1)
     # Set URI to play
-    d.playbin.props.uri = "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"
-
-    # Change state to playing
-    # Goal: start playing pipeline
-    # element / pipeline states: NULL -> READY -> PAUSED -> PLAYING.
-    # When a pipeline is moved to PLAYING state, it goes through all these 4 states internally
-    # NULL : Default start(and end) state. No resources allocated
-    # READY : Global resources allocated -> opening devices, buffer allocation. Stream is not opened
-    # PAUSED: Stream opened, but not being processed
-    # PLAYING: Stream opened and processing happens -> running clock
-    ret = d.playbin.set_state(Gst.State.PLAYING)
+    data.playbin.props.uri = "https://www.freedesktop.org/software/gstreamer-sdk/data/media/sintel_trailer-480p.webm"
+    ret = data.playbin.set_state(Gst.State.PLAYING)
     if ret == Gst.StateChangeReturn.FAILURE:
         logger.error("Unable to change state of pipeline to playing")
         sys.exit(1)
@@ -98,28 +79,28 @@ if __name__ == '__main__':
     # Why is a bus required?
     # Bus takes care of forwarding messages from pipeline (running in a separate thread) to the application
     # Applications can avoid worrying about communicating with streaming threads / the pipeline directly.
-    d.bus = d.playbin.get_bus()
+    data.bus = data.playbin.get_bus()
     while True:
-        msg = d.bus.timed_pop_filtered(100 * Gst.MSECOND, \
+        msg = data.bus.timed_pop_filtered(100 * Gst.MSECOND, \
         Gst.MessageType.STATE_CHANGED | Gst.MessageType.ERROR |  \
         Gst.MessageType.EOS | Gst.MessageType.DURATION_CHANGED)
         if msg:
-            d.handle_message(msg)
+            handle_message(msg)
         else:
-            if d.playing :
-                ret, current = d.playbin.query_position(Gst.Format.TIME)
+            if data.playing :
+                ret, current = data.playbin.query_position(Gst.Format.TIME)
                 if not ret:
                     logger.error("Could not query current position")
-                if d.duration != Gst.CLOCK_TIME_NONE:
-                    ret, d.duration = d.playbin.query_duration(Gst.Format.TIME)
+                if data.duration != Gst.CLOCK_TIME_NONE:
+                    ret, data.duration = data.playbin.query_duration(Gst.Format.TIME)
                     if not ret:
                         logger.error("Could not query duration")
                 # Print current position and total duration
-                print(f"Current Position: {current} Total Duration: {d.duration}", end="\r", flush=True)
+                print(f"Current Position: {current} Total Duration: {data.duration}", end="\r", flush=True)
 
-                if d.seek_enabled and not d.seek_done and current > 10 * Gst.SECOND:
+                if data.seek_enabled and not data.seek_done and current > 10 * Gst.SECOND:
                     print("\nReached 10s, performing seek ...\n")
-                    d.playbin.seek_simple(Gst.Format.TIME, \
+                    data.playbin.seek_simple(Gst.Format.TIME, \
                     Gst.SeekFlags.FLUSH | Gst.SeekFlags.KEY_UNIT,\
                      30 * Gst.SECOND)
                      #Gst.SeekFlags.FLUSH - This discards all data currently in the pipeline before doing the seek.
@@ -132,7 +113,15 @@ if __name__ == '__main__':
                      # If this flag is not used, the pipeline will move internally to the closest key frame (it has no other alternative)
                      # but data will not be shown until it reaches the requested position.
                      # This last alternative is more accurate, but might take longer.
-                    d.seek_done = True
-        if d.terminate:
-            d.playbin.set_state(Gst.State.NULL)
+                    data.seek_done = True
+        if data.terminate:
+            data.playbin.set_state(Gst.State.NULL)
             break
+
+
+if __name__ == '__main__':
+    # Initialize GStreamer
+    # Setup internal path lists, plugins, GstRegistry
+    Gst.init(None)
+    data = CustomData()
+    main()
