6d36e51643a53bcdf8393f013c69295871abd830
Meera Sundar <mirage.sundial@gmail.com>
2021-07-27 18:32:33 +0530

    add print_caps function

--- basic-tut-6.py
+++ basic-tut-6.py
@@ -14,6 +14,20 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
+def print_caps(caps, prefix):
+    if caps.is_any():
+        print(f"{prefix}ANY\n")
+        return
+    if caps.is_empty():
+        print(f"{prefix}EMPTY\n")
+        return
+
+    for i, val in enumerate(caps):
+        structure = caps.get_structure(i)
+        print(f"{prefix} {Gst.Structure.get_name(structure)}")
+        # pending:gst_structure_foreach (structure, print_field, (gpointer) pfx);
+
+
 def print_pad_templates_info(factory):
     print(f"Pad templates for {factory}")  # gst_element_factory_get_longname pending
     if not Gst.ElementFactory.get_num_pad_templates(factory):
@@ -22,37 +36,30 @@ def print_pad_templates_info(factory):
     pads = Gst.ElementFactory.get_static_pad_templates(
         factory
     )  # pads is a list of padtemplates
-    print("Pads", pads)
     for pad in pads:
         padtemplate = pad.get()
-        # pad = GLib.List.next(pad) #GLib.List is a struct used to represent each element in the doubly linked list
-
         if padtemplate.direction == Gst.PadDirection.SRC:
-            logger.info(f"SRC template {padtemplate.name_template}\n")
+            print(f"SRC template {padtemplate.name_template}\n")
         elif padtemplate.direction == Gst.PadDirection.SINK:
-            logger.info(f"SINK template {padtemplate.name_template}\n")
+            print(f"SINK template {padtemplate.name_template}\n")
         else:
-            logger.info(f"UNKNOWN template {padtemplate.name_template}\n")
+            print(f"UNKNOWN template {padtemplate.name_template}\n")
 
         if padtemplate.presence == Gst.PadPresence.ALWAYS:
-            logger.info("      Availability: Always\n")
+            print("      Availability: Always\n")
         elif padtemplate.presence == Gst.PadPresence.SOMETIMES:
-            logger.info("       Availability: Sometimes\n")
+            print("       Availability: Sometimes\n")
         elif padtemplate.presence == Gst.PadPresence.REQUEST:
-            logger.info("       Availability: On Request\n")
+            print("       Availability: On Request\n")
         else:
-            logger.info("       Availability: Unknown\n")
+            print("       Availability: Unknown\n")
 
-        # Pending:
-        """
-            if (padtemplate->static_caps.string) {
-          GstCaps *caps;
-          g_print ("    Capabilities:\n");
-          caps = gst_static_caps_get (&padtemplate->static_caps);
-          print_caps (caps, "      ");
-          gst_caps_unref (caps);
 
-         }"""
+        if padtemplate.get_caps():
+            print("    Capabilities:\n")
+            caps = padtemplate.get_caps()
+            print_caps(caps, "         ")
+            print(" ")
 
 
 def main():
