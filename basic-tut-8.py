47ab08535bb8134ff8f0dd1036b3e96339095afb
Meera Sundar <mirage.sundial@gmail.com>
2021-08-04 14:49:52 +0530

    add basic prog structure for tut 8

---
+++ basic-tut-8.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import sys
+import logging
+import gi
+import signal
+
+gi.require_version("GLib", "2.0")
+gi.require_version("GObject", "2.0")
+gi.require_version("Gst", "1.0")
+from gi.repository import GLib, GObject, Gst
+
+logging.basicConfig(
+    level=logging.DEBUG, format="[%(name)s] [%(levelname)8s] - %(message)s"
+)
+logger = logging.getLogger(__name__)
