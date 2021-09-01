06da2d66cc201232c0582c41c2782e274e494072
Meera Sundar <mirage.sundial@gmail.com>
2021-09-01 18:56:58 +0530

    .

--- C-code/basic-tut8.c
+++ C-code/basic-tut8.c
@@ -1,10 +1,11 @@
 #include<gst/gst.h>
 #include<gst/audio/audio.h>
+#include <string.h>
 
 #define SAMPLE_RATE 44100
 #define CHUNK_SIZE 1024
 
-typedef struct CustomData{
+typedef struct _CustomData{
   GstElement *app_source;
   GstElement *tee;
   GstElement *audio_queue;
@@ -12,18 +13,52 @@ typedef struct CustomData{
   GstElement *audio_resample;
   GstElement *audio_sink;
   GstElement *video_queue;
-  GstElement *audioconvert2;
+  GstElement *audio_convert2;
   GstElement *visual;
   GstElement *video_convert;
   GstElement *video_sink;
   GstElement *app_queue;
   GstElement *app_sink;
   GstElement *pipeline;
-  GMainLoop *main_loop
+  GMainLoop *main_loop;
   guint64 num_samples;
   gfloat a, b, c, d;
   guint sourceid;
 } CustomData;
+
+static gboolean pushdata(CustomData *data){
+  GstBuffer *buffer;
+  GstFlowReturn ret;
+  GstMapInfo map;
+  gint16 *raw;
+  gint num_samples = CHUNK_SIZE / 2; // Each sample is 16 bits = 2 bytes
+  gfloat freq;
+
+  buffer = gst_buffer_new_and_alloc(CHUNK_SIZE);
+  //Timestamp and duration for buffer
+  GST_BUFFER_TIMESTAMP(buffer) = gst_util_uint64_scale(data->num_samples, GST_SECOND, SAMPLE_RATE);
+  GST_BUFFER_DURATION(buffer) = gst_util_uint64_scale(num_samples, GST_SECOND, SAMPLE_RATE);
+
+  // Generating waveforms
+  gst_buffer_map (buffer, &map, GST_MAP_WRITE);
+  raw = (gint16*)map.data;
+  data->c += data->d;
+  data->d -= data->c / 1000;
+  freq = 1100 + 1000 * data->d;
+  for(int i = 0; i < num_samples; i++){
+    data->a += data->b;
+    data->b -= data->a / freq;
+    raw[i] = (gint16)(500 * data->a);
+  }
+
+  g_signal_emit_by_name(data->app_source, "push-buffer", buffer, &ret);
+
+  if (ret != GST_FLOW_OK){
+    return FALSE;
+  }
+  gst_buffer_unref(buffer);
+  return TRUE;
+}
 
 static void start_feed(GstElement *source, guint size, CustomData *data){
   if(data->sourceid == 0){
@@ -50,6 +85,18 @@ static gboolean new_sample(GstElement *sink, CustomData *data){
   }
   return FALSE;
 }
+static void error_cb(GstBus *bus, GstMessage *msg, CustomData *data){
+  GError *err;
+  gchar *debug_info;
+
+  gst_message_parse_error(msg, &err, &debug_info);
+  g_printerr ("Error received from element %s: %s\n", GST_OBJECT_NAME (msg->src), err->message);
+  g_printerr ("Debugging information: %s\n", debug_info ? debug_info : "none");
+  g_clear_error (&err);
+  g_free (debug_info);
+
+  g_main_loop_quit (data->main_loop);
+}
 
 
 int main(int argc, char *argv[]){
@@ -64,15 +111,15 @@ int main(int argc, char *argv[]){
   data.audio_sink = gst_element_factory_make("autoaudiosink", "audio_sink");
   data.video_queue = gst_element_factory_make("queue", "video_queue");
   data.audio_convert2 = gst_element_factory_make("audioconvert", "audio_convert2");
-  data.visual = gst_element_factory_make("waveform", "visual");
+  data.visual = gst_element_factory_make("wavescope", "visual");
   data.video_convert = gst_element_factory_make("videoconvert", "video_convert");
   data.video_sink = gst_element_factory_make("autovideosink", "video_sink");
   data.app_queue = gst_element_factory_make("queue", "app_queue");
   data.app_sink = gst_element_factory_make("appsink", "app_sink");
   data.pipeline = gst_pipeline_new("test-pipeline");
 
-if(!data.app_source || !data.tee || !data.audio_queue || !data.audio_convert1 || !data.audio_resample || !data.audio_sink || !data.video_queue || !data.audio_convert2 || !data.visual || !data.video_convert2 || !data.video_sink || !data.app_queue || !data.app_sink || !data.pipeline){
-  g_printerr("All Elements could not be created \n")
+if(!data.app_source || !data.tee || !data.audio_queue || !data.audio_convert1 || !data.audio_resample || !data.audio_sink || !data.video_queue || !data.audio_convert2 || !data.visual || !data.video_convert || !data.video_sink || !data.app_queue || !data.app_sink || !data.pipeline){
+  g_printerr("All Elements could not be created \n");
   return -1;
 }
   // configure wavescope visual
@@ -100,19 +147,20 @@ gboolean link2 = gst_element_link_many(data.audio_queue, data.audio_convert1, da
 gboolean link3 = gst_element_link_many(data.video_queue, data.audio_convert2, data.visual, data.video_convert, data.video_sink, NULL);
 gboolean link4 = gst_element_link_many(data.app_queue, data.app_sink, NULL);
 
-if(link1 || link2 || link3 || link4){
+if(!link1 || !link2 || !link3 || !link4){
   g_printerr("Elements could not be linked \n");
   return -1;
 }
 
 //Manual linking with tee pads
-GstPad *tee_audio_pad = gst_element_request_pad_simple(data.tee, "src_%u");
+GstPad *tee_audio_pad;
+tee_audio_pad = gst_element_get_request_pad(data.tee, "src_%u");
 GstPad *queue_audio_pad = gst_element_get_static_pad(data.audio_queue, "sink");
 
-GstPad *tee_video_pad = gst_element_request_pad_simple(data.tee, "src_%u");
+GstPad *tee_video_pad = gst_element_get_request_pad(data.tee, "src_%u");
 GstPad *queue_video_pad = gst_element_get_static_pad(data.video_queue, "sink");
 
-GstPad *tee_app_pad = gst_element_request_pad_simple(data.tee, "src_%u");
+GstPad *tee_app_pad = gst_element_get_request_pad(data.tee, "src_%u");
 GstPad *queue_app_pad = gst_element_get_static_pad(data.app_queue, "sink");
 gst_pad_link(tee_audio_pad, queue_audio_pad);
 gst_pad_link(tee_video_pad, queue_video_pad);
@@ -139,6 +187,12 @@ g_main_loop_run(data.main_loop);
 
 
 // Freeing resources
+gst_element_release_request_pad (data.tee, tee_audio_pad);
+gst_element_release_request_pad (data.tee, tee_video_pad);
+gst_element_release_request_pad (data.tee, tee_app_pad);
+gst_object_unref (tee_audio_pad);
+gst_object_unref (tee_video_pad);
+gst_object_unref (tee_app_pad);
 gst_element_set_state(data.pipeline, GST_STATE_NULL);
 gst_object_unref(data.pipeline);
 
