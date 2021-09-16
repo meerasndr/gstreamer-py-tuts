b2401b78055769fcd3114d059c2fabd53054f84e
Meera Sundar <mirage.sundial@gmail.com>
2021-09-16 15:14:52 +0530

    add video-res.c

--- C-code/video-res.c
+++ C-code/video-res.c
@@ -1,3 +1,4 @@
+// Compilation: gcc video-res.c -o video-res `pkg-config --cflags --libs gstreamer-1.0 gstreamer-video-1.0`
 #include<gst/gst.h>
 #include<gst/video/video.h>
 #include<string.h>
@@ -12,6 +13,9 @@ typedef struct _CustomData{
   GstElement *appsrc;
   GstElement *videoconvert;
   GstElement *autovideosink;
+  GstElement *vp8enc;
+  GstElement *matromux;
+  GstElement *filesink;
   GstElement *pipeline;
   GMainLoop *main_loop;
   GstClockTime buftimestamp;
@@ -28,7 +32,7 @@ static gboolean pushdata(CustomData *data){
   gfloat freq;
 
   if(data->framecount  ==  100){
-    gst_video_info_set_format(&(data->info), GST_VIDEO_FORMAT_GBR, 520, 420); // debugging: print width and height
+    gst_video_info_set_format(&(data->info), GST_VIDEO_FORMAT_GBR, 480, 420);
     GstCaps *video_caps2 = gst_video_info_to_caps(&(data->info));
     g_object_set(data -> appsrc, "caps", video_caps2, NULL);
   }
@@ -120,12 +124,15 @@ int main(int argc, char *argv[]){
 
   data.appsrc = gst_element_factory_make("appsrc", "video_source");
   data.videoconvert = gst_element_factory_make("videoconvert", "videoconvert");
+  data.vp8enc = gst_element_factory_make("vp8enc", "vp8enc");
+  data.matromux = gst_element_factory_make("webmmux", "webmmux");
   data.autovideosink = gst_element_factory_make("autovideosink", "autovideosink");
+  data.filesink = gst_element_factory_make("filesink", "filesink");
   data.pipeline = gst_pipeline_new("test-pipeline");
 
   gst_video_info_init(&(data.info));
 
-  if(!data.appsrc || !data.videoconvert || !data.autovideosink || !data.pipeline){
+  if(!data.appsrc || !data.videoconvert || !data.autovideosink || !data.pipeline || !data.vp8enc || !data.matromux || !data.filesink){
     g_printerr("All elements could not be created\n");
     return -1;
   }
@@ -133,52 +140,40 @@ int main(int argc, char *argv[]){
   //configure appsrc caps
   gst_video_info_set_format(&(data.info), GST_VIDEO_FORMAT_GBR, 1024, 768);
   GstCaps *video_caps1 = gst_video_info_to_caps(&(data.info));
-
-  //data.height = 768;
-  //data.width = 1024;
   data.buftimestamp = 0; //start time for buffer
   data.framecount = 0;
-// Caps filter
-/*GstCaps *video_caps = gst_caps_new_simple ("video/x-raw",
-     "format", G_TYPE_STRING, "RGB",
-     "framerate", GST_TYPE_FRACTION, 25, 1,
-     "pixel-aspect-ratio", GST_TYPE_FRACTION, 1, 1,
-     "width", G_TYPE_INT, 1024,
-     "height", G_TYPE_INT, 768,
-     NULL);
-if(!gst_video_info_from_caps(data.info, video_caps)){
-  g_printerr("Could not parse caps to Gst video info\n");
-}*/
 
-g_object_set(data.appsrc, "caps", video_caps1, "format", GST_FORMAT_TIME, NULL);
-// configure appsrc signal handler
-g_signal_connect(data.appsrc, "need-data", G_CALLBACK(start_feed), &data);
-g_signal_connect(data.appsrc, "enough-data", G_CALLBACK(stop_feed), &data);
+  g_object_set(data.appsrc, "caps", video_caps1, "format", GST_FORMAT_TIME, NULL);
+  // configure appsrc signal handler
+  g_signal_connect(data.appsrc, "need-data", G_CALLBACK(start_feed), &data);
+  g_signal_connect(data.appsrc, "enough-data", G_CALLBACK(stop_feed), &data);
 
-// Add elements to be linked
-gst_bin_add_many(GST_BIN(data.pipeline), data.appsrc, data.videoconvert, data.autovideosink, NULL);
-if(!gst_element_link_many(data.appsrc, data.videoconvert, data.autovideosink, NULL)){
-  g_printerr("Not all elements could be linked");
-  return -1;
-}
+  g_object_set(data.filesink, "location", "vidoutput", NULL);
 
-//Connect bus to error-handler callback
-//Signals to notify application of error events
-GstBus *bus = gst_element_get_bus(data.pipeline);
-gst_bus_add_signal_watch(bus);
-g_signal_connect(G_OBJECT(bus), "message::error", (GCallback)error_cb, &data);
-gst_object_unref(bus);
+  // Add elements to be linked
+  gst_bin_add_many(GST_BIN(data.pipeline), data.appsrc, data.videoconvert, data.vp8enc, data.matromux, data.filesink, NULL);
+  if(!gst_element_link_many(data.appsrc, data.videoconvert, data.vp8enc, data.matromux, data.filesink, NULL)){
+    g_printerr("Not all elements could be linked\n");
+    return -1;
+  }
 
-//Start playing pipeline
-gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
+  //Connect bus to error-handler callback
+  //Signals to notify application of error events
+  GstBus *bus = gst_element_get_bus(data.pipeline);
+  gst_bus_add_signal_watch(bus);
+  g_signal_connect(G_OBJECT(bus), "message::error", (GCallback)error_cb, &data);
+  gst_object_unref(bus);
 
-//GLib main loop
-data.main_loop = g_main_loop_new(NULL, FALSE);
-g_main_loop_run(data.main_loop);
+  //Start playing pipeline
+  gst_element_set_state(data.pipeline, GST_STATE_PLAYING);
 
-// free resources
-gst_element_set_state(data.pipeline, GST_STATE_NULL);
-gst_object_unref(data.pipeline);
+  //GLib main loop
+  data.main_loop = g_main_loop_new(NULL, FALSE);
+  g_main_loop_run(data.main_loop);
 
-return 0;
+  // free resources
+  gst_element_set_state(data.pipeline, GST_STATE_NULL);
+  gst_object_unref(data.pipeline);
+
+  return 0;
 }
